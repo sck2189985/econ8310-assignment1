15c2e7e9c1445bb84fd59d809ceb33629ed77194
sck2189985 <shelbykrz1@gmail.com>
2026-02-02 00:04:40 -0600

    test 4

--- assignment1.py
+++ assignment1.py
@@ -8,8 +8,8 @@ data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main
 data = data[['Timestamp', 'trips']].rename(columns={'Timestamp': 'ds', 'trips': 'y'}).assign(ds=lambda df: pd.to_datetime(df['ds']))
 
 model = Prophet()
-model.add_seasonality(name='weekly')
-model.add_seasonality(name='daily')
+model.add_seasonality(name='weekly', period=7, fourier_order=5)
+model.add_seasonality(name='daily', period=1, fourier_order=11)
 modelFit = model.fit(data)
 
 future = model.make_future_dataframe(periods=744)
